tuffnatty/fontforge

commit da2d65c3497438f37a945d44628ab7e5f46de7c6

Convert to initializing with SplinePointCreate()

fontforge/cvimages.c
@@ -542,23 +542,19 @@ static SplineSet * slurpelipse(FILE *fig,SplineChar *sc, SplineSet *sofar) {
 
     spl = chunkalloc(sizeof(SplinePointList));
     spl->next = sofar;
-    spl->first = sp = chunkalloc(sizeof(SplinePoint));
-    sp->me.x = dcx; sp->me.y = dcy+dry;
+    spl->first = sp = SplinePointCreate(dcx, dcy+dry);
 	sp->nextcp.x = sp->me.x + .552*drx; sp->nextcp.y = sp->me.y;
 	sp->prevcp.x = sp->me.x - .552*drx; sp->prevcp.y = sp->me.y;
-    spl->last = sp = chunkalloc(sizeof(SplinePoint));
-    sp->me.x = dcx+drx; sp->me.y = dcy;
+    spl->last = sp = SplinePointCreate(dcx+drx, dcy);
 	sp->nextcp.x = sp->me.x; sp->nextcp.y = sp->me.y - .552*dry;
 	sp->prevcp.x = sp->me.x; sp->prevcp.y = sp->me.y + .552*dry;
     SplineMake3(spl->first,sp);
-    sp = chunkalloc(sizeof(SplinePoint));
-    sp->me.x = dcx; sp->me.y = dcy-dry;
+    sp = SplinePointCreate(dcx, dcy-dry);
 	sp->nextcp.x = sp->me.x - .552*drx; sp->nextcp.y = sp->me.y;
 	sp->prevcp.x = sp->me.x + .552*drx; sp->prevcp.y = sp->me.y;
     SplineMake3(spl->last,sp);
     spl->last = sp;
-    sp = chunkalloc(sizeof(SplinePoint));
-    sp->me.x = dcx-drx; sp->me.y = dcy;
+    sp = SplinePointCreate(dcx-drx, dcy);
 	sp->nextcp.x = sp->me.x; sp->nextcp.y = sp->me.y + .552*dry;
 	sp->prevcp.x = sp->me.x; sp->prevcp.y = sp->me.y - .552*dry;
     SplineMake3(spl->last,sp);
@@ -634,8 +630,7 @@ static SplineSet * slurppolyline(FILE *fig,SplineChar *sc, SplineSet *sofar) {
 	    SplineMake3(spl->last,sp); spl->last = sp;
 	} else {
 	    for ( i=0; i<cnt; ++i ) {
-		sp = chunkalloc(sizeof(SplinePoint));
-		sp->me = sp->nextcp = sp->prevcp = bps[i];
+		sp = SplinePointCreate(bps[i].x, bps[i].y);
 		sp->pointtype = pt_corner;
 		if ( spl->first==NULL )
 		    spl->first = sp;
@@ -760,17 +755,18 @@ static SplineSet *ApproximateXSpline(struct xspline *xs,int order2) {
     FitPoint mids[7];
     SplineSet *spl = chunkalloc(sizeof(SplineSet));
     SplinePoint *sp;
+    BasePoint tbp;
 
-    spl->first = spl->last = chunkalloc(sizeof(SplinePoint));
-    xsplineeval(&spl->first->me,0,xs);
+    xsplineeval(&tbp,0,xs);
+    spl->first = spl->last = SplinePointCreate(tbp.x, tbp.y);
     spl->first->pointtype = ( xs->s[0]==0 )?pt_corner:pt_curve;
     for ( i=0; i<(size_t)(xs->n-1); ++i ) {
 	if ( i==(size_t)(xs->n-2) && xs->closed )
 	    sp = spl->first;
 	else {
-	    sp = chunkalloc(sizeof(SplinePoint));
 	    sp->pointtype = ( xs->s[i+1]==0 )?pt_corner:pt_curve;
-	    xsplineeval(&sp->me,i+1,xs);
+	    xsplineeval(&tbp,i+1,xs);
+	    sp = SplinePointCreate(tbp.x, tbp.y);
 	}
 	for ( j=0, t=1./8; j<sizeof(mids)/sizeof(mids[0]); ++j, t+=1./8 ) {
 	    xsplineeval((BasePoint *) &mids[j].p,i+t,xs);

fontforge/freetype.c
@@ -553,9 +553,7 @@ static int FT_MoveTo(const FT_Vector *to,void *user) {
     if ( context->orig_cpl!=NULL )
 	context->orig_sp = context->orig_cpl->first;
 
-    context->last = context->cpl->first = chunkalloc(sizeof(SplinePoint));
-    context->last->me.x = to->x*context->scalex;
-    context->last->me.y = to->y*context->scaley;
+    context->last = context->cpl->first = SplinePointCreate(to->x*context->scalex, to->y*context->scaley);
     if ( context->orig_sp==NULL )
 	context->last->ttfindex = -2;
     else {

fontforge/fvcomposite.c
@@ -2430,10 +2430,7 @@ static void DoSpaces(SplineFont *sf,SplineChar *sc,int layer,BDFFont *bdf,int di
 }
 
 static SplinePoint *MakeSP(real x, real y, SplinePoint *last,int order2) {
-    SplinePoint *new = chunkalloc(sizeof(SplinePoint));
-
-    new->me.x = x; new->me.y = y;
-    new->prevcp = new->nextcp = new->me;
+    SplinePoint *new = SplinePointCreate(x, y);
     new->pointtype = pt_corner;
     if ( last!=NULL )
 	SplineMake(last,new,order2);

fontforge/fvfonts.c
@@ -1310,14 +1310,11 @@ return( head );
 }
 
 static void InterpPoint(SplineSet *cur, SplinePoint *base, SplinePoint *other, real amount ) {
-    SplinePoint *p = chunkalloc(sizeof(SplinePoint));
+    SplinePoint *p = SplinePointCreate(base->me.x + amount*(other->me.x-base->me.x),
+                                       base->me.y + amount*(other->me.y-base->me.y));
     int order2 = base->prev!=NULL ? base->prev->order2 : base->next!=NULL ? base->next->order2 : false;
 
-    p->me.x = base->me.x + amount*(other->me.x-base->me.x);
-    p->me.y = base->me.y + amount*(other->me.y-base->me.y);
-    if ( order2 && base->prev!=NULL && (base->prev->islinear || other->prev->islinear ))
-	p->prevcp = p->me;
-    else {
+    if ( !( order2 && base->prev!=NULL && (base->prev->islinear || other->prev->islinear) ) ) {
 	p->prevcp.x = base->prevcp.x + amount*(other->prevcp.x-base->prevcp.x);
 	p->prevcp.y = base->prevcp.y + amount*(other->prevcp.y-base->prevcp.y);
 	if ( order2 && cur->first!=NULL ) {
@@ -1328,9 +1325,7 @@ static void InterpPoint(SplineSet *cur, SplinePoint *base, SplinePoint *other, r
 	    cur->last->nextcp.y = p->prevcp.y = (cur->last->nextcp.y+p->prevcp.y)/2;
 	}
     }
-    if ( order2 && base->next!=NULL && (base->next->islinear || other->next->islinear ))
-	p->nextcp = p->me;
-    else {
+    if ( ! ( order2 && base->next!=NULL && (base->next->islinear || other->next->islinear ) ) ) {
 	p->nextcp.x = base->nextcp.x + amount*(other->nextcp.x-base->nextcp.x);
 	p->nextcp.y = base->nextcp.y + amount*(other->nextcp.y-base->nextcp.y);
     }

fontforge/parsepdf.c
@@ -1445,9 +1445,9 @@ static void _InterpretPdf(FILE *in, struct pdfcontext *pc, EntityChar *ec) {
 		current.x = stack[sp-2].u.val;
 		current.y = stack[sp-1].u.val;
 		sp -= 2;
-		pt = chunkalloc(sizeof(SplinePoint));
-		Transform(&pt->me,&current,transform);
-		pt->prevcp = pt->me; pt->nextcp = pt->me;
+		BasePoint ini_me;
+		Transform(&ini_me,&current,transform);
+		pt = SplinePointCreate(ini_me.x, ini_me.y);
 		if ( tok==pt_moveto ) {
 		    SplinePointList *spl = chunkalloc(sizeof(SplinePointList));
 		    spl->first = spl->last = pt;
@@ -1487,10 +1487,10 @@ static void _InterpretPdf(FILE *in, struct pdfcontext *pc, EntityChar *ec) {
 		current = to;
 		if ( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) ) {
 		    Transform(&cur->last->nextcp,&ncp,transform);
-		    pt = chunkalloc(sizeof(SplinePoint));
+		    BasePoint ini_me;
+		    Transform(&ini_me,&current,transform);
+		    pt = SplinePointCreate(ini_me.x, ini_me.y);
 		    Transform(&pt->prevcp,&pcp,transform);
-		    Transform(&pt->me,&current,transform);
-		    pt->nextcp = pt->me;
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
 		}

fontforge/parsettf.c
@@ -1872,8 +1872,7 @@ static SplineSet *ttfbuildcontours(int path_cnt,uint16 *endpt, char *flags,
 	sp = NULL;
 	while ( i<=endpt[path] ) {
 	    if ( flags[i]&_On_Curve ) {
-		sp = chunkalloc(sizeof(SplinePoint));
-		sp->me = sp->nextcp = sp->prevcp = pts[i];
+		sp = SplinePointCreate(pts[i].x, pts[i].y);
 		sp->ttfindex = i;
 		sp->nextcpindex = 0xffff;
 		if ( last_off && cur->last!=NULL ) {
@@ -1883,10 +1882,7 @@ static SplineSet *ttfbuildcontours(int path_cnt,uint16 *endpt, char *flags,
 	    } else if ( last_off ) {
 		/* two off curve points get a third on curve point created */
 		/* half-way between them. Now isn't that special */
-		sp = chunkalloc(sizeof(SplinePoint));
-		sp->me.x = (pts[i].x+pts[i-1].x)/2;
-		sp->me.y = (pts[i].y+pts[i-1].y)/2;
-		sp->nextcp = sp->prevcp = sp->me;
+		sp = SplinePointCreate((pts[i].x+pts[i-1].x)/2, (pts[i].y+pts[i-1].y)/2);
 		sp->ttfindex = 0xffff;
 		sp->nextcpindex = i;
 		if ( last_off && cur->last!=NULL )
@@ -1912,19 +1908,13 @@ static SplineSet *ttfbuildcontours(int path_cnt,uint16 *endpt, char *flags,
 	    /*  point. What on earth do they think that means? */
 	    /* Oh. I see. It's used to possition marks and such */
 	    if ( cur->first==NULL ) {
-		sp = chunkalloc(sizeof(SplinePoint));
-		sp->me.x = pts[start].x;
-		sp->me.y = pts[start].y;
-		sp->nextcp = sp->prevcp = sp->me;
+		sp = SplinePointCreate(pts[start].x, pts[start].y);
 		sp->ttfindex = i-1;
 		sp->nextcpindex = 0xffff;
 		cur->first = cur->last = sp;
 	    }
 	} else if ( !(flags[start]&_On_Curve) && !(flags[i-1]&_On_Curve) ) {
-	    sp = chunkalloc(sizeof(SplinePoint));
-	    sp->me.x = (pts[start].x+pts[i-1].x)/2;
-	    sp->me.y = (pts[start].y+pts[i-1].y)/2;
-	    sp->nextcp = sp->prevcp = sp->me;
+	    sp = SplinePointCreate((pts[start].x+pts[i-1].x)/2, (pts[start].y+pts[i-1].y)/2);
 	    sp->ttfindex = 0xffff;
 	    sp->nextcpindex = start;
 	    FigureControls(cur->last,sp,&pts[i-1],is_order2);

fontforge/psread.c
@@ -733,6 +733,7 @@ static void circlearcto(real a1, real a2, real cx, real cy, real r,
 	SplineSet *cur, real *transform ) {
     SplinePoint *pt;
     DBasePoint temp, base, cp;
+    BasePoint ocp;
     real cplen;
     int sign=1;
     real s1, s2, c1, c2;
@@ -745,8 +746,8 @@ return;
     s1 = sin(a1); s2 = sin(a2); c1 = cos(a1); c2 = cos(a2);
     temp.x = cx+r*c2; temp.y = cy+r*s2;
     base.x = cx+r*c1; base.y = cy+r*s1;
-    pt = chunkalloc(sizeof(SplinePoint));
-    Transform(&pt->me,&temp,transform);
+    Transform(&ocp,&temp,transform);
+    pt = SplinePointCreate(ocp.x, ocp.y);
     cp.x = temp.x-cplen*s2; cp.y = temp.y + cplen*c2;
     if ( (cp.x-base.x)*(cp.x-base.x)+(cp.y-base.y)*(cp.y-base.y) >
 	     (temp.x-base.x)*(temp.x-base.x)+(temp.y-base.y)*(temp.y-base.y) ) {
@@ -754,7 +755,6 @@ return;
 	cp.x = temp.x+cplen*s2; cp.y = temp.y - cplen*c2;
     }
     Transform(&pt->prevcp,&cp,transform);
-    pt->nextcp = pt->me;
     cp.x = base.x + sign*cplen*s1; cp.y = base.y - sign*cplen*c1;
     Transform(&cur->last->nextcp,&cp,transform);
     CheckMake(cur->last,pt);
@@ -1271,6 +1271,7 @@ return;		/* Hunh. I don't understand it. I give up */
 static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
     SplinePointList *cur=NULL, *head=NULL;
     DBasePoint current, temp;
+    BasePoint ini_me;
     int tok, i, j;
     struct psstack stack[100];
     real dval;
@@ -2041,9 +2042,8 @@ static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
 		    current.y = stack[sp-1].u.val;
 		    sp -= 2;
 		}
-		pt = chunkalloc(sizeof(SplinePoint));
-		Transform(&pt->me,&current,transform);
-		pt->prevcp = pt->nextcp = pt->me;
+		Transform(&ini_me,&current,transform);
+		pt = SplinePointCreate(ini_me.x, ini_me.y);
 		if ( tok==pt_moveto || tok==pt_rmoveto ) {
 		    SplinePointList *spl = chunkalloc(sizeof(SplinePointList));
 		    spl->first = spl->last = pt;
@@ -2077,11 +2077,10 @@ static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
 		if ( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) ) {
 		    temp.x = stack[sp-6].u.val; temp.y = stack[sp-5].u.val;
 		    Transform(&cur->last->nextcp,&temp,transform);
-		    pt = chunkalloc(sizeof(SplinePoint));
+		    Transform(&ini_me,&current,transform);
+		    pt = SplinePointCreate(ini_me.x, ini_me.y);
 		    temp.x = stack[sp-4].u.val; temp.y = stack[sp-3].u.val;
 		    Transform(&pt->prevcp,&temp,transform);
-		    Transform(&pt->me,&current,transform);
-		    pt->nextcp = pt->me;
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
@@ -2103,9 +2102,8 @@ static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
 		temp.y = cy+r*sin(a1/180 * FF_PI);
 		if ( temp.x!=current.x || temp.y!=current.y ||
 			!( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) )) {
-		    pt = chunkalloc(sizeof(SplinePoint));
-		    Transform(&pt->me,&temp,transform);
-		    pt->prevcp = pt->nextcp = pt->me;
+		    Transform(&ini_me,&temp,transform);
+		    pt = SplinePointCreate(ini_me.x, ini_me.y);
 		    if ( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) ) {
 			CheckMake(cur->last,pt);
 			SplineMake3(cur->last,pt);
@@ -2146,9 +2144,8 @@ static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
 			(current.x-x1)*(y2-y1) == (x2-x1)*(current.y-y1) ) {
 		    /* Degenerate case */
 		    current.x = x1; current.y = y1;
-		    pt = chunkalloc(sizeof(SplinePoint));
-		    Transform(&pt->me,&current,transform);
-		    pt->prevcp = pt->nextcp = pt->me;
+		    Transform(&ini_me,&current,transform);
+		    pt = SplinePointCreate(ini_me.x, ini_me.y);
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
@@ -2179,9 +2176,8 @@ static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
 		    if ( xt1!=current.x || yt1!=current.y ) {
 			DBasePoint temp;
 			temp.x = xt1; temp.y = yt1;
-			pt = chunkalloc(sizeof(SplinePoint));
-			Transform(&pt->me,&temp,transform);
-			pt->prevcp = pt->me; pt->nextcp = pt->me;
+			Transform(&ini_me,&temp,transform);
+			pt = SplinePointCreate(ini_me.x, ini_me.y);
 			CheckMake(cur->last,pt);
 			SplineMake3(cur->last,pt);
 			cur->last = pt;
@@ -3628,6 +3624,7 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
     SplinePointList *cur=NULL, *oldcur=NULL;
     RefChar *r1, *r2, *rlast=NULL;
     DBasePoint current;
+    BasePoint ini_me;
     real dx, dy, dx2, dy2, dx3, dy3, dx4, dy4, dx5, dy5, dx6, dy6;
     SplinePoint *pt;
     /* subroutines may be nested to a depth of 10 */
@@ -3964,19 +3961,16 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 			    cur = oldcur;
 			    if ( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) ) {
 				cur->last->nextcp = old_nextcp;
-				pt = chunkalloc(sizeof(SplinePoint));
+				pt = SplinePointCreate(mid.x, mid.y);
 			        pt->hintmask = pending_hm; pending_hm = NULL;
 				pt->prevcp = mid_prevcp;
-				pt->me = mid;
 				pt->nextcp = mid_nextcp;
 				/*pt->flex = pops[2];*/
 			        CheckMake(cur->last,pt);
 				SplineMake3(cur->last,pt);
 				cur->last = pt;
-				pt = chunkalloc(sizeof(SplinePoint));
+				pt = SplinePointCreate(end.x, end.y);
 				pt->prevcp = end_prevcp;
-				pt->me = end;
-				pt->nextcp = pt->me;
 			        CheckMake(cur->last,pt);
 				SplineMake3(cur->last,pt);
 				cur->last = pt;
@@ -3986,9 +3980,7 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 			    /* Um, something's wrong. Let's just draw a line */
 			    /* do the simple method, which consists of creating */
 			    /*  the appropriate line */
-			    pt = chunkalloc(sizeof(SplinePoint));
-			    pt->me.x = pops[1]; pt->me.y = pops[0];
-			    pt->prevcp = pt->nextcp = pt->me;
+			    pt = SplinePointCreate(pops[1], pops[0]);
 			    SplinePointListFree(oldcur->next); oldcur->next = NULL; spl = NULL;
 			    cur = oldcur;
 			    if ( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) ) {
@@ -4184,12 +4176,11 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 		    current.x = rint((current.x+dx)*1024)/1024; current.y = rint((current.y+dy)*1024)/1024;
 		    cur->last->nextcp.x = current.x; cur->last->nextcp.y = current.y;
 		    current.x = rint((current.x+dx2)*1024)/1024; current.y = rint((current.y+dy2)*1024)/1024;
-		    pt = chunkalloc(sizeof(SplinePoint));
-		    pt->hintmask = pending_hm; pending_hm = NULL;
-		    pt->prevcp.x = current.x; pt->prevcp.y = current.y;
+		    ini_me.x = current.x; ini_me.y = current.y; // Store for setting prevcp;
 		    current.x = rint((current.x+dx3)*1024)/1024; current.y = rint((current.y+dy3)*1024)/1024;
-		    pt->me.x = current.x; pt->me.y = current.y;
-		    pt->nextcp = pt->me;
+		    pt = SplinePointCreate(current.x, current.y);
+		    pt->hintmask = pending_hm; pending_hm = NULL;
+		    pt->prevcp = ini_me;
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
@@ -4197,11 +4188,10 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 		    current.x = rint((current.x+dx4)*1024)/1024; current.y = rint((current.y+dy4)*1024)/1024;
 		    cur->last->nextcp.x = current.x; cur->last->nextcp.y = current.y;
 		    current.x = rint((current.x+dx5)*1024)/1024; current.y = rint((current.y+dy5)*1024)/1024;
-		    pt = chunkalloc(sizeof(SplinePoint));
-		    pt->prevcp.x = current.x; pt->prevcp.y = current.y;
+		    ini_me.x = current.x; ini_me.y = current.y; // Store for setting prevcp;
 		    current.x = rint((current.x+dx6)*1024)/1024; current.y = rint((current.y+dy6)*1024)/1024;
-		    pt->me.x = current.x; pt->me.y = current.y;
-		    pt->nextcp = pt->me;
+		    pt = SplinePointCreate(current.x, current.y);
+		    pt->prevcp = ini_me;
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
@@ -4418,10 +4408,8 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 		}
 		++polarity;
 		current.x = rint((current.x+dx)*1024)/1024; current.y = rint((current.y+dy)*1024)/1024;
-		pt = chunkalloc(sizeof(SplinePoint));
+		pt = SplinePointCreate(current.x, current.y);
 		pt->hintmask = pending_hm; pending_hm = NULL;
-		pt->me.x = current.x; pt->me.y = current.y;
-		pt->prevcp = pt->nextcp = pt->me;
 		if ( v==4 || v==21 || v==22 ) {
 		    if ( cur!=NULL && cur->first==cur->last && cur->first->prev==NULL && is_type2 ) {
 			/* Two adjacent movetos should not create single point paths */
@@ -4455,10 +4443,8 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 	    while ( sp>base+6 ) {
 		current.x = rint((current.x+stack[base++])*1024)/1024; current.y = rint((current.y+stack[base++])*1024)/1024;
 		if ( cur!=NULL ) {
-		    pt = chunkalloc(sizeof(SplinePoint));
+		    pt = SplinePointCreate(current.x, current.y);
 		    pt->hintmask = pending_hm; pending_hm = NULL;
-		    pt->me.x = current.x; pt->me.y = current.y;
-		    pt->prevcp = pt->nextcp = pt->me;
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
@@ -4537,12 +4523,11 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 		    current.x = rint((current.x+dx)*1024)/1024; current.y = rint((current.y+dy)*1024)/1024;
 		    cur->last->nextcp.x = current.x; cur->last->nextcp.y = current.y;
 		    current.x = rint((current.x+dx2)*1024)/1024; current.y = rint((current.y+dy2)*1024)/1024;
-		    pt = chunkalloc(sizeof(SplinePoint));
-		    pt->hintmask = pending_hm; pending_hm = NULL;
-		    pt->prevcp.x = current.x; pt->prevcp.y = current.y;
+		    ini_me.x = current.x; ini_me.y = current.y; // Store for setting prevcp;
 		    current.x = rint((current.x+dx3)*1024)/1024; current.y = rint((current.y+dy3)*1024)/1024;
-		    pt->me.x = current.x; pt->me.y = current.y;
-		    pt->nextcp = pt->me;
+		    pt = SplinePointCreate(current.x, current.y);
+		    pt->hintmask = pending_hm; pending_hm = NULL;
+		    pt->prevcp = ini_me;
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;
@@ -4552,10 +4537,8 @@ SplineChar *PSCharStringToSplines(uint8 *type1, int len, struct pscontext *conte
 	    if ( v==24 ) {
 		current.x = rint((current.x+stack[base++])*1024)/1024; current.y = rint((current.y+stack[base++])*1024)/1024;
 		if ( cur!=NULL ) {	/* In legal code, cur can't be null here, but I got something illegal... */
-		    pt = chunkalloc(sizeof(SplinePoint));
+		    pt = SplinePointCreate(current.x, current.y);
 		    pt->hintmask = pending_hm; pending_hm = NULL;
-		    pt->me.x = current.x; pt->me.y = current.y;
-		    pt->prevcp = pt->nextcp = pt->me;
 		    CheckMake(cur->last,pt);
 		    SplineMake3(cur->last,pt);
 		    cur->last = pt;

fontforge/sfd.c
@@ -4126,9 +4126,7 @@ static SplineSet *SFDGetSplineSet(FILE *sfd,int order2) {
 		current.x = stack[sp-2];
 		current.y = stack[sp-1];
 		sp -= 2;
-		pt = chunkalloc(sizeof(SplinePoint));
-		pt->me = current;
-		pt->prevcp = pt->nextcp = pt->me;
+		pt = SplinePointCreate(current.x, current.y);
 		if ( ch=='m' ) {
 		    SplinePointList *spl = chunkalloc(sizeof(SplinePointList));
 		    spl->first = spl->last = pt;
@@ -4158,11 +4156,9 @@ static SplineSet *SFDGetSplineSet(FILE *sfd,int order2) {
 		if ( cur!=NULL && cur->first!=NULL && (cur->first!=cur->last || cur->first->next==NULL) ) {
 		    cur->last->nextcp.x = stack[sp-6];
 		    cur->last->nextcp.y = stack[sp-5];
-		    pt = chunkalloc(sizeof(SplinePoint));
+		    pt = SplinePointCreate(current.x, current.y);
 		    pt->prevcp.x = stack[sp-4];
 		    pt->prevcp.y = stack[sp-3];
-		    pt->me = current;
-		    pt->nextcp = pt->me;
 		    if ( cur->last->nextcpindex==0xfffe )
 			cur->last->nextcpindex = ttfindex++;
 		    else if ( cur->last->nextcpindex!=0xffff )

commit c12b564830c46481bf3ab359f8665d40daff9014

fontforge/cvimages.c: Xfig import: Fix initialization-before-allocation error introduced in commit da2d65c (#4685)

fontforge/cvimages.c
@@ -767,9 +767,9 @@ static SplineSet *ApproximateXSpline(struct xspline *xs,int order2) {
 	if ( i==(size_t)(xs->n-2) && xs->closed )
 	    sp = spl->first;
 	else {
-	    sp->pointtype = ( xs->s[i+1]==0 )?pt_corner:pt_curve;
 	    xsplineeval(&tbp,i+1,xs);
 	    sp = SplinePointCreate(tbp.x, tbp.y);
+	    sp->pointtype = ( xs->s[i+1]==0 )?pt_corner:pt_curve;
 	}
 	for ( j=0, t=1./8; j<sizeof(mids)/sizeof(mids[0]); ++j, t+=1./8 ) {
 	    xsplineeval((BasePoint *) &mids[j].p,i+t,xs);